ae72f06fb7001122724eb2f00071b2638d911137
ilievladiulian <vlad.iulian.ilie@gmail.com>
2019-07-06 22:54:16 +0300

    - changed utility script to save balanced dataset - removed unlabeled entries

--- dataset/save_dataset.py
+++ dataset/save_dataset.py
@@ -2,21 +2,48 @@ import csv
 from database_connection import collection
 from news_model import NewsObject
 
-MAX_LINE_COUNT = 20000
-dataset_file = 'news_cleaned.csv'
+MAX_LINE_COUNT = 2000
+data_tags = {
+    'fake': 0, 
+    'satire': 0, 
+    'bias': 0, 
+    'conspiracy': 0, 
+    'junksci': 0, 
+    'hate': 0, 
+    'clickbait': 0, 
+    'unreliable': 0, 
+    'political': 0, 
+    'reliable': 0
+}
+dataset_file = './dataset/news_cleaned.csv'
 
 with open(dataset_file, mode='r', encoding='UTF-8') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     line_count = 0
     for row in csv_reader:
         if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
+            line_count = line_count + 1
+            continue
         else:
-            if line_count > MAX_LINE_COUNT:
+            isFull = True
+            for key, value in data_tags.items():
+                if value < MAX_LINE_COUNT:
+                    isFull = False
+            if isFull:
                 break
+
+            tag = row['type']
+            try:
+                mock = data_tags[tag]
+            except:
+                continue
+
+            if data_tags[tag] >= MAX_LINE_COUNT:
+                continue
+            else:
+                print("insert: " + tag)
+                data_tags[tag] = data_tags[tag] + 1
+                document = NewsObject(row['content'], tag)
+                document_id = collection.insert_one(document.__dict__)
             
-            document = NewsObject(row['content'], row['type'])
-            document_id = collection.insert_one(document.__dict__)
-            line_count += 1
     print(f'Processed {line_count} lines.')
