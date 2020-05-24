b6416cade04d1ae99491947cd453ac16796460e9
ilievladiulian <vlad.iulian.ilie@gmail.com>
2020-05-24 18:11:41 +0300

    Added GAN

---
+++ dataset/json_to_csv.py
@@ -0,0 +1,49 @@
+from database_connection import collection
+import csv
+import random
+
+LABEL = 'label'
+CONTENT = 'content'
+
+csv_columns = [LABEL, CONTENT]
+
+train_file_name = 'train.csv'
+test_file_name = 'test.csv'
+labels_file_name = 'labels.txt'
+
+labels = collection.distinct('label')
+labels_map = {}
+no_examples = {}
+
+for i in range(len(labels)):
+    labels_map[labels[i]] = str(i)
+    no_examples[labels[i]] = 0
+
+with open(labels_file_name, 'w') as labels_file:
+    for label in labels:
+        labels_file.write('%s\n' % label)
+
+train_examples = []
+test_examples = []
+
+for document in collection.find():
+    current_label = document[LABEL]
+    current_doc = { LABEL: labels_map[current_label], CONTENT: document[CONTENT].replace('\n', '').replace('\t', '') }
+    no_examples[current_label] += 1
+    if no_examples[current_label] <= 7000:
+        train_examples.append(current_doc)
+    else:
+        test_examples.append(current_doc)
+
+random.shuffle(train_examples)
+random.shuffle(test_examples)
+
+with open(train_file_name, 'w') as train_file:
+    writer = csv.DictWriter(train_file, fieldnames=csv_columns)
+    for example in train_examples:
+        writer.writerow(example)
+
+with open(test_file_name, 'w') as test_file:
+    writer = csv.DictWriter(test_file, fieldnames=csv_columns)
+    for example in test_examples:
+        writer.writerow(example)
--- env_settings.py
+++ env_settings.py
@@ -1,5 +1,23 @@
 import torch
+import gensim
 
 CUDA_DEVICE = 0
 
 device = torch.cuda.device(CUDA_DEVICE)
+
+def get_embedding_weights(embedding):
+    model = None
+    if embedding == 'glove_specific':
+        model = gensim.models.KeyedVectors.load_word2vec_format('./specific-embeddings/glove.vec')
+    elif embedding == 'glove_generic':
+        model = gensim.models.KeyedVectors.load_word2vec_format('./.vector_cache/glove.6B.300d.txt')
+    elif embedding == 'fasttext_specific':
+        model = gensim.models.KeyedVectors.load_word2vec_format('./specific-embeddings/fasttext.vec')
+    elif embedding == 'fasttext_generic':
+        model = gensim.models.KeyedVectors.load_word2vec_format('./.fasttext-cache/crawl-300d-2M.vec')
+    elif embedding == 'word2vec_specific':
+        model = gensim.models.KeyedVectors.load_word2vec_format('./specific-embeddings/word2vec.vec')
+    elif embedding == 'word2vec_generic':
+        model = gensim.models.KeyedVectors.load_word2vec_format('./.word2vec_cache/embeddings.vec')
+
+    return torch.nn.Parameter(torch.FloatTensor(model.vectors), requires_grad=False)
